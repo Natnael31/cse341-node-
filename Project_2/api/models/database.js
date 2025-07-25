afbdb1d8a121683dbe611fbda697dc16979ff152
Natnael31 <end24002byui.edu>
2025-07-25 15:05:15 +0300

    update

--- Project_2/api/models/database.js
+++ Project_2/api/models/database.js
@@ -1,35 +1,3 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// require('dotenv').config();
-
-// let client;
-
-// const initDb = async () => {
-//     if (!process.env.MONGODB_URI) {
-//         throw new Error("MONGODB_URI not found in .env");
-//     }
-
-//     client = new MongoClient(process.env.MONGODB_URI, {
-//         serverApi: {
-//             version: ServerApiVersion.v1,
-//             strict: true,
-//             deprecationErrors: true,
-//         },
-//     });
-
-//     await client.connect();
-//     console.log("Database connected!");
-//     return client;
-// };
-
-// const getClient = () => {
-//     if (!client) {
-//         throw new Error("DB client not initialized. Call initDb() first.");
-//     }
-//     return client;
-// };
-
-// module.exports = { initDb, getClient };
-
 const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
